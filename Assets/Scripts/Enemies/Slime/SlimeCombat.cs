8e9bf0838e00e29035ec9d8dfe83538bc97d0b6e
Naffei <justintran32@hotmail.com>
2024-02-27 23:59:12 -0600

    Added Push to Enemy
    Player can now attack along with slime being pushed from attack

--- Assets/Scripts/Enemies/Slime/SlimeCombat.cs
+++ Assets/Scripts/Enemies/Slime/SlimeCombat.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SlimeC : MonoBehaviour
 {
+//     public int hp = 100;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+                
     }
+
 }
